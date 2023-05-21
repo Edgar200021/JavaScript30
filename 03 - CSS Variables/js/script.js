520e790cafea6e1c4598efa68762eda9b8582248
Edgar <edgarartur2000@mail.ru>
2023-05-21 12:54:41 +0300

    feat:finished CSS variables project

--- 03 - CSS Variables/js/script.js
+++ 03 - CSS Variables/js/script.js
@@ -1,1 +1,30 @@
 const inputs = document.querySelectorAll('.controls input')
+const controls = document.querySelector('.controls')
+
+function handleUpdate() {
+
+//? Solution with changing wars
+
+  const sufix = this.dataset['sizing'] || 'px'
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+
+// ? Solution without vars
+//  const img = document.querySelector('img')
+
+//  if (this.id === 'spacing') {
+//    img.style.padding = `${this.value}px`
+//  }
+
+//  if (this.id === 'blur') {
+//    img.style.filter = `blur(${this.value}px)`
+//  }
+
+//  if (this.id === 'base') {
+//    img.style.backgroundColor = this.value
+//  }
+}
+
+
+inputs.forEach((input) => {
+  input.addEventListener('input', handleUpdate)
+})
